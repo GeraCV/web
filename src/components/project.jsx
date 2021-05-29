5dab3a86b548c9f29df3547ef95f2a0128fafbea
Gerardo CV <gerardocarrizosavalentin1a@gmail.com>
2021-05-29 01:14:08 -0500

    Diseño mobile terminado.

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -7,8 +7,10 @@ const Project = ({ src, title, rp, rc }) => {
         <img className="image-project" src={src} alt={title} />
       </div>
       <div className="container-buttons">
+        <div className="buttons">
         <a href={rp} className="link"> Ver proyecto </a>
         <a href={rc} className="link"> Ver código </a>
+        </div>
       </div>
     </div>
   )
